502515c7b4b4724e9af6569cb74c26ac0f9928d0
Maciej Kazalski <296581@student.agh.edu.pl>
2022-05-15 01:04:57 +0200

    Initial widget complete

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -3,21 +3,28 @@ import Plot from "./Plot";
 import PropTypes from "prop-types";
 import { Container, Alert, AlertTitle, Grid, Paper } from "@mui/material";
 import Deposits from "../components/Deposits";
+import GraphDetails from "./GraphDetails.js";
 
-function Graph({
-  currentTab,
-  graphs,
-  sortIntoGrid,
-  configLoaded
-}) {
+function Graph({ currentTab, graphs, sortIntoGrid, configLoaded }) {
   if (!configLoaded) {
-    return (<Grid></Grid>)
+    return <Grid></Grid>;
   }
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+    <Container maxWidth="false" sx={{ mt: 4, mb: 4 }}>
       {graphs.map((graph) => (
         <Grid container spacing={3} sx={{ m: 1 }}>
-          <Grid item xs={12} md={8} lg={9}>
+          <Grid item xs={12} md={3} lg={3}>
+            <Paper
+              sx={{
+                p: 3,
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <GraphDetails chart={graph} />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={7} lg={7}>
             <Paper
               sx={{
                 p: 3,
@@ -28,7 +35,7 @@ function Graph({
               <Plot key={graph.title} chart={graph} />
             </Paper>
           </Grid>
-          <Grid item xs={12} md={4} lg={3}>
+          <Grid item xs={12} md={2} lg={2}>
             <Paper
               sx={{
                 p: 3,
