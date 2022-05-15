c21c7faa869503d1562ef77f07cadae644fdb5fb
Maciej Kazalski <296581@student.agh.edu.pl>
2022-05-15 23:16:56 +0200

    Fixed layout after hiding widget

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -21,7 +21,7 @@ function Graph({
     <Container maxWidth="false" sx={{ mt: 4, mb: 4 }}>
       {graphs.map((graph) => (
         <Grid container spacing={3} sx={{ m: 1 }}>
-          <Grid item xs={12} md={8} lg={9}>
+          <Grid item xs={12} md={8} lg={datesToggled ? 12 : 9}>
             <Paper
               sx={{
                 p: 3,
@@ -32,28 +32,29 @@ function Graph({
               <Plot key={graph.title} chart={graph} />
             </Paper>
           </Grid>
-          <Grid item xs={12} md={3} lg={3}>
-            <Paper
-              sx={{
-                p: 3,
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <GraphDetails chart={graph} configLoaded={configLoaded} />
-            </Paper>
-            <Paper
-              sx={{
-                visibility: datesToggled ? "hidden" : "block",
-                p: 3,
-                mt: 3,
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <Deposits />
-            </Paper>
-          </Grid>
+          {!datesToggled && (
+            <Grid item xs={12} md={3} lg={3}>
+              <Paper
+                sx={{
+                  p: 3,
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <GraphDetails chart={graph} configLoaded={configLoaded} />
+              </Paper>
+              <Paper
+                sx={{
+                  p: 3,
+                  mt: 3,
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Deposits />
+              </Paper>
+            </Grid>
+          )}
         </Grid>
       ))}
     </Container>
